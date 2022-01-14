cdc1d2a29ebe23602637e09bf7a599a0edce164f
Yuriy <and-1991@list.ru>
2022-01-14 11:22:25 +0300

    added select

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,8 @@ import {format} from 'date-fns';
 import {groq} from "next-sanity";
 import {useState} from "react";
 import Dialog from '@mui/material/Dialog';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
 
 // стартовый экран
 export default function Post(props) {
@@ -21,6 +23,7 @@ export default function Post(props) {
 
   const [selectedImg, setSelectedImg] = useState(null);
   const [isOpen, setIsOpen] = useState(false);
+  const [selectedPost, setSelectedPost] = useState(undefined);
 
   const openDialog = url => {
     setSelectedImg(url);
@@ -34,29 +37,55 @@ export default function Post(props) {
     }, 200)
   }
 
+  const handleChange = (event) => {
+    setSelectedPost(event.target.value);
+  };
+
   const getContentBody = (array) => Array.isArray(array) ? array.map(el => el.text) : null;
 
   return (
     <>
+      <div className="p-5">Selected Post</div>
+      <div className="pr-5 pl-5">
+        <Select
+          autoWidth
+          value={selectedPost}
+          onChange={handleChange}
+        >
+          {posts && posts.map((post, idx) => (
+            <MenuItem key={idx} value={post.title}>
+              {post.title}
+            </MenuItem>
+          ))}
+        </Select>
+      </div>
       {posts &&
         posts.map((post, idx) => (
           <article className="p-5" key={idx}>
-            <div className=" w-full lg:max-w-full lg:flex text-center">
+            <div className="w-full lg:max-w-full lg:flex text-center">
               <div className=" w-full border border-gray-400 p-5 flex flex-col justify-between leading-normal">
                 <div className="mb-8">
-                  <div className="text-gray-900 font-bold text-xl mb-2">{post.title}</div>
+                  {selectedPost === post.title && (
+                    <div className="text-xs p-1 bg-amber-50">
+                      You selected this post
+                    </div>
+                  )}
+                  <div
+                    className={`font-bold text-xl mb-2 ${selectedPost === post.title ? "text-red-500" : 'text-gray-900'}`}>
+                    {post.title}
+                  </div>
                   <p className="text-gray-700 text-base">
                     {post.body.map((el, key) => (
-                      <div key={key}>{getContentBody(el.children)}</div>
+                      <span key={key}>{getContentBody(el.children)}</span>
                     ))}
                   </p>
                 </div>
                 {post.mainImage?.asset?.url && (
-                  <div className="block h-64 relative rounded leading-snug">
+                  <div className="flex h-64 relative rounded leading-snug justify-center">
                     {/* eslint-disable-next-line @next/next/no-img-element */}
                     <img
                       onClick={() => openDialog(post.mainImage?.asset?.url)}
-                      className="w-full h-full rounded-r object-contain cursor-pointer"
+                      className="h-full rounded-r object-contain cursor-pointer"
                       src={post.mainImage.asset.url}
                       alt="image"
                     />
